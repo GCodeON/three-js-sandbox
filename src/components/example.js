96013d148b70f303ac4c096d1d01d9e832651104
GCodeON <gcodeondev@gmail.com>
2024-01-15 18:26:29 -0800

    clean up example

--- src/components/example.js
+++ src/components/example.js
@@ -12,40 +12,16 @@ export default function Example() {
     const group = new THREE.Group();
     scene.add(group);
 
-    // Load Texture w/ JS
-    const image = new Image();
-    const blockTexture = new THREE.Texture(image);
-    blockTexture.colorSpace = THREE.SRGBColorSpace;
-
-    image.onload = () => {
-      blockTexture.needsUpdate = true;
-    }
-
-    image.src = '/question-block.png';
-
-    
+    // Texture Loader
+    const textureLoader = new THREE.TextureLoader();
+    const blockTexture = textureLoader.load('/question-block.png');
 
     // Object
     const geometry = new THREE.BoxGeometry(1,1,1);
     const material = new THREE.MeshBasicMaterial({map: blockTexture});
     const cube = new THREE.Mesh(geometry, material);
     
-    // cube.position.x = -2;
-
     group.add(cube)
-
-    // const cube2 = new THREE.Mesh(
-    //   new THREE.BoxGeometry(1,1,1),
-    //   new THREE.MeshBasicMaterial({ color: 0x0000ff})
-    // )
-    // group.add(cube2);
-
-    // const cube3 = new THREE.Mesh(
-    //   new THREE.BoxGeometry(1,1,1),
-    //   new THREE.MeshBasicMaterial({ color: 0x00ff00})
-    // )
-    // cube3.position.x = 2;
-    // group.add(cube3);
 
     // Camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -55,13 +31,11 @@ export default function Example() {
     const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // Helpers
-    const axesHelper = new THREE.AxesHelper();
-    scene.add(axesHelper);
+    // // Helpers
+    // const axesHelper = new THREE.AxesHelper();
+    // scene.add(axesHelper);
 
     // Resize
-
-
     window.addEventListener( 'resize', () => {
       renderer.setSize( window.innerWidth, window.innerHeight );
       camera.aspect = window.innerWidth / window.innerHeight;
@@ -79,15 +53,8 @@ export default function Example() {
       cursor.y = event.clientY / window.innerHeight - 0.5;
     })
 
-    const clock = new THREE.Clock();
-
     const animate = () => {
-      // const elapsedTime = clock.getElapsedTime();
-
-      // group.rotation.x = Math.sin(elapsedTime);
-      // group.position.y = Math.cos(elapsedTime);
-      // group.position.x = Math.sin(elapsedTime);
-
+  
       camera.position.x = cursor.x * 10;
       camera.position.y = cursor.y * 10;
       camera.lookAt(new THREE.Vector3());
